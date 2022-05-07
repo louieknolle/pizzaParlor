cbc93e856dc97b80503bc603ec63ef5857961e36
Louis Knolle <knollelw@gmail.com>
2022-05-06 22:20:08 -0400

    pass test for calculateOrderTotal function

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Describe PizzaOrder()
 Test: "It should return an object for the pizza order containing the size and toppings as properties."
 Code: const newPizza = new PizzaOrder(['sweet potato'. 'pepperoni'], 'large');
 Expected output: PizzaOrder {toppings: ['sweet potato', 'pepperoni'], size: 'large'}
+
+
+Describe calculateOrderTotal()
+
+Test: "It should determine base price for order based on size."
+Code: if(pizzaSize = 'small') {orderTotal = 12.99}
+Expected output: newPizza = {toppings:'', size: 'small'}, orderTotal = 12.99
+
+Test: "It should add 1.50 to orderTotal for each additional topping."
+Code: if(pizzaToppings = 1) {orderTotal += 1.50}
+Expected output = newPizza = {toppings: 'pepperoni', size: 'small'}, orderTotal = 14.49
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,38 @@
 //Business Logic
 function PizzaOrder(size, toppings) {
-  this.size = size;
-  this.toppings = toppings;
+  this.sizeOfPizza = size;
+  this.pizzaToppings = toppings;
   this.calculateOrderTotal();
 }
 
-PizzaOrder.prototype.calculateOrderTotal = function()
+PizzaOrder.prototype.calculateOrderTotal = function() {
+  let orderTotal = 0;
+
+  if (this.sizeOfPizza === 'small') {
+    orderTotal = 12.99;
+  } else if (this.sizeOfPizza === 'medium') {
+    orderTotal = 16.99;
+  } else {
+    orderTotal = 20.99;
+  }
+  
+  if (this.pizzaToppings === 1) {
+    orderTotal += 1.50;
+  } else if (this.pizzaToppings === 2) {
+    orderTotal += 3;
+  } else if (this.pizzaToppings === 3) {
+    orderTotal += 4.50;
+  } else {
+    orderTotal;
+  }
+  return orderTotal;
+};
 
 
 // UI Logic
 $(document).ready(function() {
-  
+  $('form#pizzaOrderInputForm').submit(function(event) {
+    event.preventDefault();
+    const pizzaSize = $
+  });
 });
